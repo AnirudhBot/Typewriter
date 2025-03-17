b697344f83e94e485d61b4c037f3989e3ba6475f
Anirudh Sharma <ansharma@konradgroup.com>
2025-03-17 18:19:18 +0530

    quill error debug

--- frontend/src/components/editor/Editor.tsx
+++ frontend/src/components/editor/Editor.tsx
@@ -36,12 +36,16 @@ const Editor = () => {
         setUserPermission(document?.permissions?.find(p => p.user === user?.id));
     }, [document, user]);
 
-    console.log("hi");
-    console.log(editorRef?.current);
+    console.log(loading);
+    console.log(error);
     console.log(document);
+    console.log(editorRef.current);
 
     useEffect(() => {
         if (loading || error || !document || !editorRef.current) return;
+        console.log("hi");
+        console.log(editorRef?.current);
+        console.log(document);
 
         const quill = new Quill(editorRef.current, {
             theme: 'snow',
